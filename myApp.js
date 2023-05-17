b4333607357dab5b25251ecff91b9bd03cdb8113
Ömer Ayaz <omerfayaz166@gmail.com>
2023-05-17 15:05:19 +0300

    a

--- myApp.js
+++ myApp.js
@@ -1,6 +1,10 @@
 let express = require('express');
 let app = express();
-
+app.get('/json',(req,res)=>{
+    if(process.env.MESSAGE_STYLE = "upparcase"){
+        res.json({"message":"HELLO JSON"})
+    }
+})
 
 
 
